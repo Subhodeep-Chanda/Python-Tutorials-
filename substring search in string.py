47465f8e0e5ff741c1f3a97217a14b29ae0f25ce
Subhodeep-Chanda <71210074+Subhodeep-Chanda@users.noreply.github.com>
2020-12-23 23:24:52 +0530

    Add files via upload

---
+++ substring search in string.py
@@ -0,0 +1,9 @@
+string = input("Enter a string:")
+search = input("Enter the word to be searched in the string:")
+if(string.find(search) != -1):
+    print("String found , search successful")
+else:
+    print("String not found , search unsuccessful")
+    
+    
+    
